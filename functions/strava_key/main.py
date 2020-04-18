6853654132f8a065eccdcfd9b4eceba4b991b8b1
j-penson <jpenson24@gmail.com>
2020-04-18 12:05:38 +0100

    fix: doc.set for firebase update

--- functions/strava_key/main.py
+++ functions/strava_key/main.py
@@ -41,8 +41,8 @@ def refresh_key(event, context):
 
         response_dict = response.json()
 
-        doc.update({'access_token': response_dict['access_token'],
-                    'refresh_token': response_dict['refresh_token'],
-                    'expires_at': response_dict['expires_at']})
+        doc.set({'access_token': response_dict['access_token'],
+                 'refresh_token': response_dict['refresh_token'],
+                 'expires_at': response_dict['expires_at']})
 
         print(f'updated {doc.id} with new expiry time {response_dict["expires_at"]}')
