5a375f0551ef621a82ff71ad4a67f31bac3699d0
j-penson <jpenson24@gmail.com>
2020-04-18 21:21:05 +0100

    fix: str not string

--- functions/extract_load/src/bigquery.py
+++ functions/extract_load/src/bigquery.py
@@ -6,23 +6,23 @@
 import pandas as pd
 import os
 
-SEGMENT_DTYPES = {'name': 'string', 'activity_type': 'string', 'distance': 'float64',
+SEGMENT_DTYPES = {'name': 'str', 'activity_type': 'str', 'distance': 'float64',
                   'average_grade': 'float64', 'maximum_grade': 'float64',
-                  'elevation_high': 'float64', 'elevation_low': 'float64', 'start_latlng': 'string',
-                  'end_latlng': 'string', 'start_latitude': 'float64', 'end_latitude': 'float64',
+                  'elevation_high': 'float64', 'elevation_low': 'float64', 'start_latlng': 'str',
+                  'end_latlng': 'str', 'start_latitude': 'float64', 'end_latitude': 'float64',
                   'start_longitude': 'float64', 'end_longitude': 'float64',
-                  'climb_category': 'int64', 'city': 'string', 'state': 'string', 'country': 'string',
-                  'private': 'bool', 'starred': 'bool', 'athlete_segment_stats': 'string',
-                  'created_at': 'string', 'updated_at': 'string', 'total_elevation_gain': 'float64',
-                  'map': 'string', 'effort_count': 'int64', 'athlete_count': 'int64',
-                  'hazardous': 'bool', 'star_count': 'int64', 'pr_time': 'string',
+                  'climb_category': 'int64', 'city': 'str', 'state': 'str', 'country': 'str',
+                  'private': 'bool', 'starred': 'bool', 'athlete_segment_stats': 'str',
+                  'created_at': 'str', 'updated_at': 'str', 'total_elevation_gain': 'float64',
+                  'map': 'str', 'effort_count': 'int64', 'athlete_count': 'int64',
+                  'hazardous': 'bool', 'star_count': 'int64', 'pr_time': 'str',
                   'starred_date': 'float64', 'athlete_pr_effort': 'float64'}
 
-LEADERBOARD_DTYPES = {'athlete_name': 'string', 'elapsed_time': 'string', 'moving_time': 'string',
-                      'start_date': 'string', 'start_date_local': 'string', 'rank': 'int64',
-                      'segment_name': 'string', 'segment_activity_type': 'string', 'segment_distance': 'float64',
-                      'segment_start_latlng': 'string', 'segment_end_latlng': 'string', 'entry_count': 'int64',
-                      'effort_count': 'int64', 'kom_type': 'string'}
+LEADERBOARD_DTYPES = {'athlete_name': 'str', 'elapsed_time': 'str', 'moving_time': 'str',
+                      'start_date': 'str', 'start_date_local': 'str', 'rank': 'int64',
+                      'segment_name': 'str', 'segment_activity_type': 'str', 'segment_distance': 'float64',
+                      'segment_start_latlng': 'str', 'segment_end_latlng': 'str', 'entry_count': 'int64',
+                      'effort_count': 'int64', 'kom_type': 'str'}
 
 
 def write_df(df, table):
